92cf64bb7a0bfb6269bbf6b0eff4d5e310c95977
anandpatel48 <anandrpatel48@gmail.com>
2022-04-06 15:19:27 -0400

    create openGame function

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+
+//declare player and dealer totals to be used in functions
+let playerTotal = 0
+let dealerTotal = 0
+
+//for the hit button, set allowedToHit equal to true and make it false if playerTotal is at or above 21
+let allowedToHit = true
+
+
+//declaring a function to open game so it can be called when the reset button is hit
+function openGame(){
+    makeDeck()
+    shuffle()
+}
+
+openGame()
+
+
 //making a function to make the deck so I can call back to it whenever needed
 function makeDeck(){
     //creating a deck of cards
@@ -7,7 +25,7 @@ function makeDeck(){
     //using strings because some card values are letter
     let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     //creating an empty deck so that a for loop can push card and suit pairs into the deck
-    let deck = []
+    deck = []
     //nested for loop to create the deck
     for (let i = 0; i < suits.length; i++){
         for(let j = 0; j < cards.length; j++){
@@ -15,16 +33,16 @@ function makeDeck(){
             deck.push(cards[j] + "-" + suits[i])
         }
     }
-    return deck
+
 }
 //console log makeDeck() to make sure it works
 // console.log(makeDeck())
 
-// console.log(deck)
+// console.log(makeDeck())
 
 // console.log(test)
 //create a function to shuffle deck
-function shuffle(deck){
+function shuffle(){
     for (let i = 0; i <deck.length; i++){
         //temp is the card in the deck we are replacing with the random card
         let temp = deck[i];
@@ -33,5 +51,6 @@ function shuffle(deck){
         deck[i] = deck[randomIndex];
         deck[randomIndex] = temp
     }
+    console.log(deck)
 }
 
