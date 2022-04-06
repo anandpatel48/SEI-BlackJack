02d4c471cb5c45759334e788028d005522dd7b71
anandpatel48 <anandrpatel48@gmail.com>
2022-04-06 15:06:11 -0400

    add card images

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 function makeDeck(){
     //creating a deck of cards
     //creating an array of suits
-    let suits = ['h', 'd', 's', 'c']
+    let suits = ['H', 'D', 'S', 'C']
     //creating array of card values, called cards
     //using strings because some card values are letter
-    let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']
+    let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     //creating an empty deck so that a for loop can push card and suit pairs into the deck
     let deck = []
     //nested for loop to create the deck
     for (let i = 0; i < suits.length; i++){
         for(let j = 0; j < cards.length; j++){
             // console.log(cards[j] + suits[i]);
-            deck.push(cards[j]+suits[i])
+            deck.push(cards[j] + "-" + suits[i])
         }
     }
     return deck
@@ -24,17 +24,14 @@ function makeDeck(){
 
 // console.log(test)
 //create a function to shuffle deck
-function shuffle(test){
-    for (let i = 0; i <test.length; i++){
+function shuffle(deck){
+    for (let i = 0; i <deck.length; i++){
         //temp is the card in the deck we are replacing with the random card
-        let temp = test[i];
+        let temp = deck[i];
         //randomIndex will generate a random number from 0-51
-        let randomIndex  = Math.floor(Math.random() * test.length);
-        test[i] = test[randomIndex];
-        test[randomIndex] = temp
+        let randomIndex  = Math.floor(Math.random() * deck.length);
+        deck[i] = deck[randomIndex];
+        deck[randomIndex] = temp
     }
 }
 
-let test = makeDeck()
-shuffle(test)
-console.log(test)
